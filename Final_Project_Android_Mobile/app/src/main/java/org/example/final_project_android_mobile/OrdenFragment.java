4403cba72844a0a5ac4c8c44bebfaca942115270
KendallCalderonR <Kendall-1132@hotmail.com>
2019-08-30 23:22:03 -0600

    mau

---
+++ Final_Project_Android_Mobile/app/src/main/java/org/example/final_project_android_mobile/OrdenFragment.java
@@ -0,0 +1,16 @@
+package org.example.final_project_android_mobile;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class OrdenFragment extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_orden_fragment);
+
+        
+    }
+}
